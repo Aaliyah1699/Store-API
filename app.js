cc7ece0cd805d6a8cc5ec1ab13db8420c8f1a292
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-15 20:55:48 -0500

    configure express app

---
+++ app.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+// async errors
+
+const express = require("express");
+const app = express();
+
+const notFoundMiddleware = require("./middleware/not-found");
+const errorMiddleware = require("./middleware/error-handler");
