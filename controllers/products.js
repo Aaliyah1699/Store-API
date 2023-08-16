ef273377be1abd83935c906ce66e2aa2b5e5779f
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-16 17:08:41 -0500

    skip, limit, and page setup

--- controllers/products.js
+++ controllers/products.js
@@ -1,25 +1,35 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
   const { featured, company, name, sort, fields } = req.query;
+
   const queryObject = {};
+
   // find
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
   }
+
   if (company) {
     queryObject.company = company;
   }
+
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
   //console.log(queryObject);
   let result = Product.find(queryObject);
+
   //sort
   if (sort) {
     const sortList = sort.split(",").join(" ");
@@ -33,6 +43,14 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  // limit & skip
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
