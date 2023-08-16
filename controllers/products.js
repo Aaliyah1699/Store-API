18113e07dfa6bcfaebb53e8a82ef7c6c64b00238
Aaliyah <93296412+Aaliyah1699@users.noreply.github.com>
2023-08-15 23:29:26 -0500

    fixed bug

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,9 @@
 const getAllProductsStatic = async (req, res) => {
-  res.status(200).json({ mgs: "products testing route" });
+  res.status(200).json({ msg: "products testing route" });
 };
 
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ mgs: "products route" });
+  res.status(200).json({ msg: "products route" });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
